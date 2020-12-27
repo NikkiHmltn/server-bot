1d4f68911f537e03a36e849d53a67895fdcbe000
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-12-27 14:51:09 -0800

    fixed another bug

--- src/bot.js
+++ src/bot.js
@@ -8,13 +8,6 @@ const bot = new Discord.Client({ws: {intents: Discord.Intents.ALL}})
 bot.commands = new Discord.Collection()
 const prefix = "!lore"
 
-const  commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
-
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    bot.commands.set(command.name, command)
-}
-
 bot.on('ready', () => {
     console.log(`Sayaka-bot has logged in`)
 })
