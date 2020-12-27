cb5f2c883730b9da9a1b19507515b59037ae5eb3
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-12-26 20:01:53 -0800

    changed name and deleted console log

--- src/bot.js
+++ src/bot.js
@@ -5,18 +5,14 @@ const bot = new Discord.Client({ws: {intents: Discord.Intents.ALL}})
 bot.commands = new Discord.Collection()
 
 bot.on('ready', () => {
-    console.log(`Sayaka has logged in`)
-})
-
-bot.on('message', (message) => {
-    console.log(message.content)
+    console.log(`Sayaka-bot has logged in`)
 })
 
 bot.on('guildMemberUpdate', (oldMember, newMember) => {
 
     const changeEmbed = new Discord.MessageEmbed()
         .setColor('#42ecf5')
-        .setDescription(`Kaichou, ***MILKROT***  has become ***${newMember.nickname}***. *Beep Boop*`)
+        .setDescription(`Danny-san, ***MILKROT***  has become ***${newMember.nickname}***. *Beep Boop*`)
 
     if(oldMember.id === "717816505777127514") {
         bot.channels.cache.get("729543736463458356").send(changeEmbed)
