22201c313e1bff7ef8a325b0ec8a7748867cefd7
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-12-26 19:17:13 -0800

    commiting the code

---
+++ src/bot.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const Discord = require('discord.js')
+const bot = new Discord.Client({ws: {intents: Discord.Intents.ALL}})
+bot.commands = new Discord.Collection()
+
+bot.on('ready', () => {
+    console.log(`Sayaka has logged in`)
+})
+
+bot.on('message', (message) => {
+    console.log(message.content)
+})
+
+bot.on('guildMemberUpdate', (oldMember, newMember) => {
+
+    const changeEmbed = new Discord.MessageEmbed()
+        .setColor('#42ecf5')
+        .setDescription(`Kaichou, ***MILKROT*** has become ***${newMember.nickname}***.`)
+
+    if(oldMember.id === "717816505777127514") {
+        bot.channels.cache.get("729543736463458356").send(changeEmbed)
+    }
+})
+
+bot.login(process.env.SAYAKA_BOT_TOKEN);
