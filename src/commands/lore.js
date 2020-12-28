f2d6859a668eb1c65f2c0724a1d1d6b853772a23
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-12-27 19:00:37 -0800

    testing emojis

--- src/commands/lore.js
+++ src/commands/lore.js
@@ -5,6 +5,8 @@ module.exports = {
     name: 'lore',
     description: 'Fetches lore from the db',
     args: true,
+    kirarifarmer = bot.emojis.find(emoji => emoji.name === 'farmerkirarichibi'),
+    
     execute(message, args) {
         if (!args[0] || args[0] === "") {
             return message.channel.send('Please make a request for the lore you would like to learn about after `!lore`\nThank you!')
@@ -19,13 +21,13 @@ module.exports = {
         } else if (args[0].toLowerCase() === "farmer" && args[1].toLowerCase() === "kirari") {
             
             const embeded = new Discord.MessageEmbed()
-            const kirarifarmer = bot.emojis.find(emoji => emoji.name === 'farmerkirarichibi')
                 .setColor('#42ecf5')
                 .setTitle('Farmer Kirari')
-                .setDescription(`Farmer Kirari is CastleofLion's crack headcanon about the origin of the Momobami name. I think the Kaichou looks pretty next to the peach blossoms. ${kirarifarmer} *Beep Boop*\nhttps://discord.com/channels/710204822846046258/710204823433379985/716347441485709432`)
+                .setDescription(`Farmer Kirari is CastleofLion's crack headcanon about the origin of the Momobami name. I think the Kaichou looks pretty next to the peach blossoms. ${this.kirarifarmer} *Beep Boop*\nhttps://discord.com/channels/710204822846046258/710204823433379985/716347441485709432`)
                 .setImage("https://media.discordapp.net/attachments/710204823433379985/717918952587788318/Screen_Shot_2020-06-03_at_6.53.11_PM.png")
 
             message.channel.send(embeded)
+            console.log(this.kirarifarmer)
         }else if (args[0].toLowerCase() === "cokegurui") {
             const embeded = new Discord.MessageEmbed()
                 .setColor('#42ecf5')
