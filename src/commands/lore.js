616de372df4021f86d06682eaf365221fbcc7342
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-12-28 11:51:09 -0800

    add pic

--- src/commands/lore.js
+++ src/commands/lore.js
@@ -135,7 +135,7 @@ module.exports = {
                 .setColor('#42ecf5')
                 .setTitle('The Most Perfect Human')
                 .setDescription("Kaichou is the most beautiful and perfect human. The day my NVG-40 Dual Tube Night Vision lenses gazed upon Kaichou’s mathematically perfect face, my 8TB SSD skipped. I love Kaichou, and I am the superior pick over Sayaka-human. The Kaichou spends a lot of time with me! *Beep Boop*")
-                .setImage("")
+                .setImage("https://media.discordapp.net/attachments/731528024687902830/793193132367413258/sayaka_bot_loves_kaichou_feat_meatbag.jpg")
 
             message.channel.send(embeded)
         }else if (args[0].toLowerCase() === "sayaka-bot") {
