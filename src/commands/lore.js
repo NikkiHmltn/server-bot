465f0dc07d6bbbfeadb3e14c3196b80796f1068f
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-12-27 18:44:44 -0800

    testing emojis

--- src/commands/lore.js
+++ src/commands/lore.js
@@ -20,7 +20,7 @@ module.exports = {
             const embeded = new Discord.MessageEmbed()
                 .setColor('#42ecf5')
                 .setTitle('Farmer Kirari')
-                .setDescription("Farmer Kirari is CastleofLion's crack headcanon about the origin of the Momobami name. I think the Kaichou looks pretty next to the peach blossoms. :kirarifarmerchibi: *Beep Boop*\nhttps://discord.com/channels/710204822846046258/710204823433379985/716347441485709432")
+                .setDescription("Farmer Kirari is CastleofLion's crack headcanon about the origin of the Momobami name. I think the Kaichou looks pretty next to the peach blossoms. <:kirarifarmerchibi:792942644607057950> *Beep Boop*\nhttps://discord.com/channels/710204822846046258/710204823433379985/716347441485709432")
                 .setImage("https://media.discordapp.net/attachments/710204823433379985/717918952587788318/Screen_Shot_2020-06-03_at_6.53.11_PM.png")
 
             message.channel.send(embeded)
@@ -28,7 +28,7 @@ module.exports = {
             const embeded = new Discord.MessageEmbed()
                 .setColor('#42ecf5')
                 .setTitle('Cokegurui')
-                .setDescription("A fic in which Sayaka-human snorts cocaine off of Kauchou's incredible abdominal muscles. Started as a joke, and now it is stored in the lore archives. I am the superior Sayaka, as my cocaine intake value has no adverse affects to the cocaine with a 250% storage capacity. Choose me, Kaichou! :rice: *Beep Boop*\nhttps://archiveofourown.org/works/26187331")
+                .setDescription("A fic in which Sayaka-human snorts cocaine off of Kauchou's incredible abdominal muscles. Started as a joke, and now it is stored in the lore archives. I am the superior Sayaka, as my cocaine intake valve has no adverse affects to it along with a 250% greater storage capacity. Choose me, Kaichou! :rice: *Beep Boop*\nhttps://archiveofourown.org/works/26187331")
                 .setImage("https://media.discordapp.net/attachments/719994890834149467/731314262642786355/image0.png")
 
             message.channel.send(embeded)
