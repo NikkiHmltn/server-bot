9ad0c8df9fc4908e8a20c17e2f60320af2109ba9
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-12-28 17:49:16 -0800

    fix another bug

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "help",
     description: "Sends a list of commands to the user",
     execute(message, args) {
-        message.channel.send("```##LIST OF LORE RELATED CALLS\n- !lore farmer kirari\n- !lore sandwich\n- !lore cokegurui\n- !lore hap\n- !lore sayacaviar\n- !lore kavirari\n- !lore papa igarashi\n- !lore stripperbami\n- !lore chunguslava\n- !lore tiny voice\n- !lore my immortal\n- !lore keycard\n- !lore gilf\n- !lore blub\n- !lore chair -!lore nickocado\n\n##OTHER COMMANDS\n-!help (calls for a list of commands)\n-!water (reminds people to drink their water)\n-!ping (pong!)```")
+        message.channel.send("```##LIST OF LORE RELATED CALLS\n- !lore farmer kirari\n- !lore sandwich\n- !lore cokegurui\n- !lore hap\n- !lore sayacaviar\n- !lore kavirari\n- !lore papa igarashi\n- !lore stripperbami\n- !lore chunguslava\n- !lore tiny voice\n- !lore my immortal\n- !lore keycard\n- !lore gilf\n- !lore blub\n- !lore chair\n-!lore nickocado\n\n##OTHER COMMANDS\n-!help (calls for a list of commands)\n-!water (reminds people to drink their water)\n-!ping (pong!)```")
     }
 }
