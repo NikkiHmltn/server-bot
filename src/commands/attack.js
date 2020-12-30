64e2abb40e8a8d0c70c6ce2fa1ac612b4ee4d201
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-12-30 15:42:31 -0800

    test command

--- src/commands/attack.js
+++ src/commands/attack.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "attack",
     description: "Sayaka-bot unleashes a devasting tazer blow.",
     execute(message, args) {
-        message.channel.send("Do not resist, meat bag.:zap: :zap:", {
+        message.channel.send(":zap:Do not resist, meat bag.:zap:", {
             files: ["https://media.discordapp.net/attachments/793225719731716126/793977264253042688/Screen_Shot_2020-12-30_at_3.01.55_PM.png"]
         })
     }
