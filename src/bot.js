ccedb848b3e149b0ced304c2ea306c67e08eb16b
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-01-16 10:23:42 -0800

    fixed a thing

--- src/bot.js
+++ src/bot.js
@@ -86,10 +86,10 @@ bot.on('guildMemberAdd', (member) => {
 
 bot.login("token").then(() => {
     console.log("I am ready");
-    let guild = client.guilds.cache.get('710204822846046258');
+    let guild = bot.guilds.cache.get('710204822846046258');
     if(guild && guild.member.cache.get('425792318562369536')){
             scheduledMessage = new cron.CronJob('00 25 18 * * 6', () => {
-                guild.member.cache.get('425792318562369536').send("test").then(() => client.destroy());
+                guild.member.cache.get('425792318562369536').send("test").then(() => bot.destroy());
             });
             scheduledMessage.start()
             console.log(scheduledMessage)
@@ -100,7 +100,7 @@ bot.login("token").then(() => {
         //if the bot doesn't have guild with the id guildid
         // or if the guild doesn't have the channel with id channelid
     }
-    client.destroy();
+    bot.destroy();
 });
 
 
