931d434355cf5f0f30e7b5f901051b3312527e59
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-01-16 10:26:12 -0800

    fixed a thing

--- src/bot.js
+++ src/bot.js
@@ -86,6 +86,7 @@ bot.on('guildMemberAdd', (member) => {
 
 bot.login("token").then(() => {
     console.log("I am ready");
+    console.log(bot.guilds.cache)
     let guild = bot.guilds.cache.get('710204822846046258');
     if(guild && guild.member.cache.get('425792318562369536')){
             scheduledMessage = new cron.CronJob('00 25 18 * * 6', () => {
@@ -97,6 +98,7 @@ bot.login("token").then(() => {
         console.log("nope");
         console.log(guild.member.cache)
         console.log(guild)
+        
         //if the bot doesn't have guild with the id guildid
         // or if the guild doesn't have the channel with id channelid
     }
