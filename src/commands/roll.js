3f9114384ef6047005de543a1bc2b239b628aeca
nicole <nikkihmltn1@yahoo.com>
2021-04-07 07:43:09 -0700

    added boundaries

--- src/commands/roll.js
+++ src/commands/roll.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'Flips a coin for heads or tails!',
 	execute(message, args) {
 
-        let number = Math.floor(Math.random() * 24)
+        let number = Math.floor(Math.random() * ((24-1) +1) + 1)
         console.log(number)
         message.channel.send(`-dice sounds- ${number}`)
 	},
