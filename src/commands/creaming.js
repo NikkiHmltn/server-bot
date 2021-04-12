735212ef5081fec90b2405eaa1bb3e2766e3a966
nicole <nikkihmltn1@yahoo.com>
2021-04-12 11:37:30 -0700

    set mp3 as file

--- src/commands/creaming.js
+++ src/commands/creaming.js
@@ -2,7 +2,9 @@ module.exports = {
     name: 'creaming',
     description: 'Sends out a message and the song link to dreaming creaming sisters',
     execute(message, args){
-        message.channel.send("(Ɔ ˘⌣˘):hearts:Ｋａｉｃｈｏｕ:hearts:(˘⌣˘ C) likes this song, so that is enough for me. Yumemite Yumemi and Jabami Yumeko must be persuaded to perform for her by any means necessary <:sayakataze:720068601519800450>\n  https://cdn.discordapp.com/attachments/793225719731716126/831232944378609735/Russian_Roulette_of_Love_by_The_Dreaming_Creaming_Sisters.mp3")
+        message.channel.send("(Ɔ ˘⌣˘):hearts:Ｋａｉｃｈｏｕ:hearts:(˘⌣˘ C) likes this song, so that is enough for me. Yumemite Yumemi and Jabami Yumeko must be persuaded to perform for her by any means necessary <:sayakataze:720068601519800450>", { 
+            files: ['https://cdn.discordapp.com/attachments/793225719731716126/831232944378609735/Russian_Roulette_of_Love_by_The_Dreaming_Creaming_Sisters.mp3']
+        })
 
     }
 }
