d6b719196195188c2b1461ae1cfdd29f82f42b12
nicole <nikkihmltn1@yahoo.com>
2021-06-06 17:10:32 -0700

    setting up code transfer?

--- src/commands/creaming.js
+++ src/commands/creaming.js
@@ -5,6 +5,5 @@ module.exports = {
         message.channel.send("(Ɔ ˘⌣˘):hearts:Ｋａｉｃｈｏｕ:hearts:(˘⌣˘ C) likes this song, so that is enough for me. Yumemite Yumemi and Jabami Yumeko must be persuaded to perform for her by any means necessary <:sayakataze:720068601519800450>", { 
             files: ['https://cdn.discordapp.com/attachments/793225719731716126/831232944378609735/Russian_Roulette_of_Love_by_The_Dreaming_Creaming_Sisters.mp3']
         })
-
     }
 }
