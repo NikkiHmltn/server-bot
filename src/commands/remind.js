d004e63ca172ba72342e6328455be90643f6d70a
nicole <nikkihmltn1@yahoo.com>
2021-07-21 17:20:16 -0700

    test

--- src/commands/remind.js
+++ src/commands/remind.js
@@ -10,27 +10,23 @@ module.exports = {
         let requestor = splitMessage[0]
         let filterMessage = splitMessage[1]
         let notifyPerson;
-        let timeOfMessage = splitMessage[2].split("in")
+        let timeOfMessage = splitMessage[2].split(" ")
         let timeReminder = timeOfMessage[1]
         if (requestor.includes('me')) {
             notifyPerson = message.author.id
             //run a function in here to set reminder
             //bot.message.mentions? 
-            console.log(splitMessage, " split", requestor, "requestor", filterMessage, "filterMessage", timeOfMessage, "time of", timeReminder, "time reminder")
-
-            message.channel.send(`<@${notifyPerson}> was tagged with "${filterMessage}" task in ${timeOfMessage}`)
+            console.log(splitMessage)
+            // message.channel.send(`<@${notifyPerson}> was tagged with "${filterMessage}" task in ${timeOfMessage}`)
         } else if (requestor.includes('@')) {
             let person = message.mentions.members.first()
             notifyPerson = person.id
-            console.log(splitMessage, " split", requestor, "requestor", filterMessage, "filterMessage", timeOfMessage, "time of", timeReminder, "time reminder")
-
-            message.channel.send(`<@${notifyPerson}> was tagged with "${filterMessage}" task in ${timeOfMessage}`)
+            console.log(splitMessage)
+            // message.channel.send(`<@${notifyPerson}> was tagged with "${filterMessage}" task in ${timeOfMessage}`)
         } else if (!requestor.includes('@') || !requestor.includes('me')) {
-            console.log(splitMessage, " split", requestor, "requestor", filterMessage, "filterMessage", timeOfMessage, "time of", timeReminder, "time reminder")
 
             message.channel.send('Error getting id')
         } else {
-            console.log(splitMessage, " split", requestor, "requestor", filterMessage, "filterMessage", timeOfMessage, "time of", timeReminder, "time reminder")
 
             message.channel.send('404 Not Found')
         }
