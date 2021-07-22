710ed7cfec4acbdcb62bf867c806d8721db11dfb
nicole <nikkihmltn1@yahoo.com>
2021-07-21 17:13:48 -0700

    tracking down variables, fix split

--- src/commands/remind.js
+++ src/commands/remind.js
@@ -7,21 +7,31 @@ module.exports = {
         }
         let string = args.toString()
         let splitMessage = string.split('"')
-        let filterMessage = splitMessage[0]
+        let requestor = splitMessage[0]
+        let filterMessage = splitMessage[1]
         let notifyPerson;
-        let timeOfMessage = splitMessage[2]
-        if (filterMessage.includes('me')) {
+        let timeOfMessage = splitMessage[2].split("in")
+        let timeReminder = timeOfMessage[1]
+        if (requestor.includes('me')) {
             notifyPerson = message.author.id
             //run a function in here to set reminder
             //bot.message.mentions? 
-            message.channel.send(`@<${notifyPerson}> was tagged with "${filterMessage}" task in ${timeOfMessage}`)
-        } else if (filterMessage.includes('@')) {
+            console.log(splitMessage, " split", requestor, "requestor", filterMessage, "filterMessage", timeOfMessage, "time of", timeReminder, "time reminder")
+
+            message.channel.send(`<@${notifyPerson}> was tagged with "${filterMessage}" task in ${timeOfMessage}`)
+        } else if (requestor.includes('@')) {
             let person = message.mentions.members.first()
             notifyPerson = person.id
-            message.channel.send(`@<${notifyPerson}> was tagged with "${filterMessage}" task in ${timeOfMessage}`)
-        } else if (!filterMessage.includes('@') || !filterMessage.includes('me')) {
+            console.log(splitMessage, " split", requestor, "requestor", filterMessage, "filterMessage", timeOfMessage, "time of", timeReminder, "time reminder")
+
+            message.channel.send(`<@${notifyPerson}> was tagged with "${filterMessage}" task in ${timeOfMessage}`)
+        } else if (!requestor.includes('@') || !requestor.includes('me')) {
+            console.log(splitMessage, " split", requestor, "requestor", filterMessage, "filterMessage", timeOfMessage, "time of", timeReminder, "time reminder")
+
             message.channel.send('Error getting id')
         } else {
+            console.log(splitMessage, " split", requestor, "requestor", filterMessage, "filterMessage", timeOfMessage, "time of", timeReminder, "time reminder")
+
             message.channel.send('404 Not Found')
         }
 		
